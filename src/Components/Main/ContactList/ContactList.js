7238310e6dced2fafdb8dec56d872accb9639514
Ne_prosto_Dena Rozbiynuk <kykarahcik28@gmail.com>
2022-02-08 19:27:06 +0200

    v.0.3.1

--- src/Components/Main/ContactList/ContactList.js
+++ src/Components/Main/ContactList/ContactList.js
@@ -1,9 +1,21 @@
 
 import ContactItem from "./ContactItem/ContactItem";
 
-const ContactList = () => {
+const ContactList = ({ List, onStateChange, onDelete, onGetCurrentIndex }) => {
+
+    const contact = List.map(item => {
+        return (<ContactItem key={item.Id} {...item}
+            onStateChange={() => onStateChange(item.Id)}
+            onDelete={() => onDelete(item.Id)}
+            onGetCurrentIndex={() => onGetCurrentIndex(item.Id)} />)
+    })
+
     return (
-        <ContactItem />
+
+        <section>
+            {contact.length > 0 ? contact : <p className="emptyList">Contact list is empty!</p>}
+        </section>
+
     )
 }
 
